67e40e28c2aa00b7d9cb5394b4c4dc12ce718e13
Alex Jensen <alexander.jensen@noaa.gov>
2025-09-30 15:35:25 -0700

    Added set.seed to sample_dm.R as well, in case I use that sampling option

--- R/sample_dm.R
+++ R/sample_dm.R
@@ -7,6 +7,7 @@
 #' @return A data frame with one row because right now the input
 #' data should only be a single row of data.
 sample_dm <- function(data, n, par) {
+  set.seed(10) # for given n and row of true age proportions, will generate same results
   lambda <- n / par^2 - 1
   if (lambda < 0 | is.na(lambda)) {
     return(rep(NA, length(data)))
